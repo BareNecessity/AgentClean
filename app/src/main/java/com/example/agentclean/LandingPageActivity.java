455bd281d9dd67498d44302014ed841fc5edcc96
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-10-27 17:41:15 +0100

    Click Listener

--- app/src/main/java/com/example/agentclean/LandingPageActivity.java
+++ app/src/main/java/com/example/agentclean/LandingPageActivity.java
@@ -3,12 +3,17 @@ package com.example.agentclean;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LandingPageActivity extends AppCompatActivity {
+    Button newUser, existingUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        newUser = findViewById(R.id.new_user_bt);
+        existingUser =findViewById(R.id.existing_user_bt);
     }
+
 }
