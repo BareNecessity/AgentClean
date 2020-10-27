05211016ae65f81269a099d049da7e597c879d72
Akano Adekola <akano.adekola@gmail.com>
2020-10-27 17:30:28 +0100

    Added intent to navigate to landing page from splash screen

--- app/src/main/java/com/example/agentclean/BackgroundActivity.java
+++ app/src/main/java/com/example/agentclean/BackgroundActivity.java
@@ -2,6 +2,7 @@ package com.example.agentclean;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -15,7 +16,8 @@ public class BackgroundActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                Intent intent = new Intent(getApplicationContext(), LandingPageActivity.class);
+                startActivity(intent);
             }
         }, 3000);
     }
